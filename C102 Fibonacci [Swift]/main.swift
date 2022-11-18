43dd7f06cfcf11ffa0bd0e0b87b9a57481cc234a
TahmidR2456 <tahmidr05@gmail.com>
2022-11-18 11:57:49 -0600

    launching 1157 second commit

---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,24 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func FibonacciFunction(num: Int) -> Int {
+    print("Calculating fibonacci(\(num))...")
+    if (num <= 2) {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let results = FibonacciFunction(num: num-1) + FibonacciFunction(num: num-2)
+        print("Non-terminal case, returning \(results).")
+        return results
+    }
+}
+
+print("The final result is: \(FibonacciFunction(num: n)).")
